b6dcc3c174eeb49392e63709bbbc3c6fbf2b2b9c
NX-GoldJust <117308582+NX-GoldJust@users.noreply.github.com>
2023-04-14 23:53:43 +0200

    Ajout Mute / Unmute

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -85,7 +85,91 @@ class Moderation(commands.Cog):
             
             embed = discord.Embed(title="Clear", description="Le salon a bien été recréé.")
             await new_channel.send(embed=embed, delete_after=10, content=interaction.user.mention)
-        
+
+
+
+
+
+
+
+    @app_commands.command(name="mute", description="Mute une personne")
+    @app_commands.describe(user="Qui")
+    @app_commands.choices(temps=[
+        app_commands.Choice(name="1 Minute", value="0"),
+        app_commands.Choice(name="5 Minutes", value="1"),
+        app_commands.Choice(name="10 Minutes", value="2"),
+        app_commands.Choice(name="30 Minutes", value="3"),
+        app_commands.Choice(name="45 Minutes", value="4"),
+        app_commands.Choice(name="1 Heure", value="5"),
+        app_commands.Choice(name="12 Heures", value="6"),
+        app_commands.Choice(name="1 Jour", value="7"),
+        app_commands.Choice(name="3 Jour", value="8"),
+        app_commands.Choice(name="1 Semaine", value="9")
+        ])
+    @app_commands.describe(raison="Pourquoi ?")
+    @app_commands.default_permissions(mute_members=True)
+    async def mute(self, i: discord.Interaction, user: discord.Member, temps: app_commands.Choice[str], raison: str):
+        from datetime import timedelta, datetime
+         
+        t = int(temps.value)
+        min = 0
+        heure = 0
+        day = 0
+        if t == 0:
+            min += 1
+        elif t == 1:
+            min += 5
+        elif t == 2:
+            min += 10
+            
+        elif t == 3:
+            min += 30
+            
+        elif t == 4:
+            min += 45
+            
+        elif t == 5:
+            heure += 1
+        elif t == 6:
+            heure += 12
+        elif t == 7:
+            day += 1
+        elif t == 8:
+            day += 3
+        elif t == 9:
+            day += 7
+
+        if i.user.top_role>user.top_role:
+            
+            temp = timedelta(minutes=min, hours=heure, days=day)
+            embed = discord.Embed(title="Mute", timestamp=datetime.now(), color=discord.Colour.red())
+            embed.add_field(name="User", value=f"Nom: {user.name}\nId: {user.id}", inline=False)
+            
+            time = int(int(datetime.now().timestamp())+int(temp.total_seconds()))
+            embed.add_field(name="Temps", value=f"Date d'unmute: <t:{time}>\n\nTemps restant: <t:{time}:R>")
+            embed.add_field(name="Raison", value=raison, inline=False)
+            embed.add_field(name="Modérateur", value=i.user.mention+f" ({i.user.name})", inline=False)
+            embed.add_field(name="Serveur", value=i.guild.name, inline=False)
+            
+            await user.timeout(temp)
+            await i.response.send_message(embed=embed)
+        else:
+            await i.response.send_message("Vous devez avoir un role plus haut !!", ephemeral=True)
+         
+    @app_commands.command(name="unmute", description="Unmute un membre")
+    @app_commands.default_permissions(mute_members=True)
+    @app_commands.describe(user="Qui ?")
+    async def unmute(self, i: discord.Interaction, user: discord.Member):
+        if i.user.top_role > user.top_role:
+            
+            if user.is_timed_out():
+                from datetime import timedelta
+                await user.timeout(timedelta(seconds=1))
+                await i.response.send_message(f"Le membre {user.mention} a été unmute")
+            else:
+                await i.response.send_message("Ce membre n'est pas mute !", ephemeral=True)
+        else:
+            await i.response.send_message("Vous devez avoir un role plus haut !!", ephemeral=True)
 
 
 async def setup(bot):
