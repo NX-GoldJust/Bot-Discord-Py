19c092b5ad39087148801073617568674da54a71
NX-GoldJust <117308582+NX-GoldJust@users.noreply.github.com>
2023-04-14 23:46:02 +0200

    Code complet sur main.py

--- bot.py
+++ bot.py
@@ -1,9 +1,20 @@
 
 
 #Import :
-import discord
+import discord, os
 from discord.ext import commands
 from config import *
 
 
-bot = commands.Bot(command_prefix=)
+bot = commands.Bot(command_prefix=PREFIX, intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    for cog in os.listdir("./cogs"):
+        if cog.endswith(".py"):
+            await bot.load_extension(f"cogs.{cog[:-3]}")
+    await bot.tree.sync()
+    print(f"Bot is ready as {bot.user.name}")
+
+
+@bot.run(TOKEN)
